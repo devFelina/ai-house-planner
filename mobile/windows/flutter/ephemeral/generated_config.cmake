120cb6974e83cf7aef0b6d9dfb28e6aa87ddd562
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-08-11 22:23:32 +0530

    Add files via upload

---
+++ mobile/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Desktop\\SEF_Project\\ai-house-planner\\mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\Desktop\\SEF_Project\\ai-house-planner\\mobile"
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Desktop\\SEF_Project\\ai-house-planner\\mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Desktop\\SEF_Project\\ai-house-planner\\mobile"
+  "FLUTTER_TARGET=D:\\Desktop\\SEF_Project\\ai-house-planner\\mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuMA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NTU5ZmZhM2Y3NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NGM1MjVkYWM1ZQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Desktop\\SEF_Project\\ai-house-planner\\mobile\\.dart_tool\\package_config.json"
+)
