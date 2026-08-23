7c81ffdba42e8c227a7bad40bf4066006aa12250
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-08-23 11:20:20 +0530

    feat: fixed login with google issue

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -4,9 +4,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'views/login_view.dart';
 import 'views/register_view.dart';
 import 'views/intake_view.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   // Make sure you have run `flutterfire configure` in your terminal
   // and import 'firebase_options.dart' to use the code below:
